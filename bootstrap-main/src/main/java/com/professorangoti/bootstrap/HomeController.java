0d23259e4143cbdbc8c164260fa98ac407f518fc
Lucas Ilussencio da Silva <ilussencio@gmail.com>
2022-09-15 22:17:17 -0300

    Correções durante a aula

--- bootstrap-main/src/main/java/com/professorangoti/bootstrap/HomeController.java
+++ bootstrap-main/src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @GetMapping ("home")
     public String showHome(){
         return "home";
     }
